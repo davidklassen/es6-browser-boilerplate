c1f57ab7a939b81a13b5fc5c5af90cb8d0b3b4b2
David Klassen <f0rk.tt@gmail.com>
2015-06-30 18:39:42 +0200

    Added gulp-load-plugins to load gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,40 @@
 const path = require('path');
 const gulp = require('gulp');
-const eslint = require('gulp-eslint');
-const jscs = require('gulp-jscs');
-const mocha = require('gulp-mocha');
-const istanbul = require('gulp-istanbul');
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
-const sequence = require('gulp-sequence');
 const babelify = require('babelify');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const karma = require('karma').server;
 const isparta = require('isparta');
 const babel = require('babel/register');
+const plugins = require('gulp-load-plugins')();
 
 function unitTests() {
     return gulp.src(['tests/unit/**/*.js'])
-        .pipe(mocha({
+        .pipe(plugins.mocha({
             reporter: 'spec',
-            compilers: {
-                js: babel
-            }
+            compilers: { js: babel }
         }));
 }
 
 gulp.task('lint', function () {
     return gulp.src(['src/**/*.js', 'tests/**/*.js'])
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError())
-        .pipe(jscs());
+        .pipe(plugins.eslint())
+        .pipe(plugins.eslint.format())
+        .pipe(plugins.eslint.failAfterError())
+        .pipe(plugins.jscs());
 });
 
 gulp.task('coverage', function (done) {
     gulp.src(['src/**/*.js'])
-        .pipe(istanbul({
+        .pipe(plugins.istanbul({
             instrumenter: isparta.Instrumenter,
             includeUntested: true
         }))
-        .pipe(istanbul.hookRequire())
+        .pipe(plugins.istanbul.hookRequire())
         .on('finish', function () {
             unitTests()
-                .pipe(istanbul.writeReports())
-                .pipe(istanbul.enforceThresholds({ thresholds: { global: 100 } }))
+                .pipe(plugins.istanbul.writeReports())
+                .pipe(plugins.istanbul.enforceThresholds({ thresholds: { global: 100 } }))
                 .on('end', done);
         });
 });
@@ -71,11 +63,11 @@ gulp.task('browserify', function () {
 
 gulp.task('compile', function () {
     return gulp.src('dist/lib-build.js')
-        .pipe(uglify())
-        .pipe(rename({ extname: '.min.js' }))
+        .pipe(plugins.uglify())
+        .pipe(plugins.rename({ extname: '.min.js' }))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', sequence('browserify', 'compile'));
-gulp.task('test', sequence('coverage', 'build', 'test:integration'));
-gulp.task('default', sequence('lint', 'test'));
+gulp.task('build', plugins.sequence('browserify', 'compile'));
+gulp.task('test', plugins.sequence('coverage', 'build', 'test:integration'));
+gulp.task('default', plugins.sequence('lint', 'test'));
