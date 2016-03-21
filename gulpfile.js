9078c13fb41e88b738fa247978ad9f3ba954cdc2
Valentin Konusov <rlng-krsk@yandex.ru>
2016-03-21 13:06:36 +0700

    gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const path = require('path');
 const gulp = require('gulp');
 const babelify = require('babelify');
 const browserify = require('browserify');
+const watchify = require('watchify');
 const source = require('vinyl-source-stream');
 const karma = require('karma').server;
 const isparta = require('isparta');
@@ -51,14 +52,7 @@ gulp.task('test:integration', function (done) {
 });
 
 gulp.task('browserify', function () {
-    return browserify({
-        entries: './build.js',
-        debug: true,
-        transform: [babelify]
-    })
-        .bundle()
-        .pipe(source('lib-build.js'))
-        .pipe(gulp.dest('dist'));
+    return buildScript(false);
 });
 
 gulp.task('compile', function () {
@@ -71,3 +65,38 @@ gulp.task('compile', function () {
 gulp.task('build', plugins.sequence('browserify', 'compile'));
 gulp.task('test', plugins.sequence('coverage', 'build', 'test:integration'));
 gulp.task('default', plugins.sequence('lint', 'test'));
+
+gulp.task('watch', function () {
+    buildScript(true);
+});
+
+function handleErrors() {
+    var args = Array.prototype.slice.call(arguments);
+    console.log("Error: ", args[0].loc, args[0].filename);
+    this.emit('end');
+}
+
+function buildScript(watch) {
+    var props = {
+        entries: './build.js',
+        debug : true,
+        transform: [babelify]
+    };
+
+    // watchify() if watch requested, otherwise run browserify() once
+    var bundler = watch ? watchify(browserify(props), {poll: true}) : browserify(props);
+
+    function rebundle() {
+        var stream = bundler.bundle();
+        return stream
+            .on('error', handleErrors)
+            .pipe(source('lib-build.js'))
+            .pipe(gulp.dest('dist'));
+    }
+    bundler.on('update', function() {
+        rebundle();
+        plugins.util.log('Rebundle...');
+    });
+
+    return rebundle();
+}
